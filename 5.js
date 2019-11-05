ef82ffe5a8b905515d66d4581d1a11c0e280db57
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-11-05 12:50:27 +0300

    Create 5.js

---
+++ 5.js
@@ -0,0 +1,13 @@
+//Среди трех чисел найти среднее. Если среди чисел есть равные, вывести сообщение Ошибка;.
+let num1,num2,num3;
+num1=20;
+num2=10;
+num3=30;
+if ((num1==num2)||(num1==num3)||(num2==num1)||(num2==num3)){
+    console.log ("Ошибка");
+}
+else {
+    let s=(num1+num2+num3)/3;
+    console.log(s);
+}
+
