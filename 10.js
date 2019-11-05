2b846bb93dfabb8184aeef4e6deb434ee5211143
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-11-05 12:50:19 +0300

    Create 10.js

---
+++ 10.js
@@ -0,0 +1,29 @@
+//По введенному номеру дня недели вывести его название, используя оператор
+//множественного выбора. Нумерация дней недели начинается с 1 – понедельник, 2
+//— вторник и т. д.
+let x=7;
+switch(x){
+    case 1:
+    console.log ("Понедельник");
+    break;
+    case 2:
+    console.log ("Вторник");
+    break;
+    case 3:
+    console.log ("Среда");
+    break;
+    case 4:
+    console.log ("Четверг");
+    break;
+    case 5:
+    console.log ("Пятница");
+    break;
+    case 6:
+    console.log ("Суббота");
+    break;
+    case 7:
+    console.log ("Воскресенье");
+    break;
+    default:
+    console.log ("такого дня недели нет");  
+}
