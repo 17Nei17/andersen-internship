bd74726a4d1f86a7bce00798b5a4431206428b26
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-11-05 12:50:13 +0300

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,14 @@
+
+//Определите переменные str, num, flag и txt со значениями «Привет», 123, true,
+//«true». При помощи оператора определения типа убедитесь, что переменных
+//принадлежат типам: string, number, boolean.
+'use strict';
+let str, num,flag, txt;
+str = "Привет";
+num=123;
+flag=true;
+txt="true";
+console.log(typeof(str));
+console.log(typeof(num));
+console.log(typeof(flag));
+console.log(typeof(txt));
