ae551099705a6e48f085b1c099ccf35d3023ba0c
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-11-05 14:38:15 +0300

    Update 10.js

--- 10.js
+++ 10.js
@@ -3,27 +3,27 @@
 //— вторник и т. д.
 let x=7;
 switch(x){
-    case 1:
-    console.log ("Понедельник");
-    break;
-    case 2:
-    console.log ("Вторник");
-    break;
-    case 3:
-    console.log ("Среда");
-    break;
-    case 4:
-    console.log ("Четверг");
-    break;
-    case 5:
-    console.log ("Пятница");
-    break;
-    case 6:
-    console.log ("Суббота");
-    break;
-    case 7:
-    console.log ("Воскресенье");
-    break;
-    default:
-    console.log ("такого дня недели нет");  
+  case 1:
+  console.log ("Понедельник");
+  break;
+  case 2:
+  console.log ("Вторник");
+  break;
+  case 3:
+  console.log ("Среда");
+  break;
+  case 4:
+  console.log ("Четверг");
+  break;
+  case 5:
+  console.log ("Пятница");
+  break;
+  case 6:
+  console.log ("Суббота");
+  break;
+  case 7:
+  console.log ("Воскресенье");
+  break;
+  default:
+  console.log ("такого дня недели нет");  
 }
