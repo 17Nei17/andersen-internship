759d2e69fecfc7f54154b461b00390bc1b39101f
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-11-13 15:39:25 +0300

    Create pract3-1.js

---
+++ pract3-1.js
@@ -0,0 +1,7 @@
+//Создайте функцию, принимающую на входе целое число и возвращающую количество цифр  в этом числе.
+
+function givNumeral (x){
+  let numeral = String(x);
+  console.log (`${numeral.length}`);
+}
+givNumeral(325443);
