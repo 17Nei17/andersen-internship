6119efe6886075d45d8063f1ee315117157c84f5
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-11-13 15:40:00 +0300

    Create pract3-9.js

---
+++ pract3-9.js
@@ -0,0 +1,21 @@
+/*9. Создайте функцию filter(arr, func) , которая получает массив arr и возвращает новый, 
+в который входят только те элементы arr , для которых func возвращает true.
+ .. ваш код для filter
+var arr = [1, 2, 3, 4, 5, 6, 7];
+console.log( filter(arr, function(a) { return a % 2 == 0 }) ); // 2,4,6
+*/
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
+
+function filter(arr, func){
+  let newArr=[];
+  let filt = func;
+  for (let i=0;i<arr.length; i++){
+    if (filt(arr[i])===true){
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+console.log( filter(arr, function(a) { return a % 2 == 0 }) );
