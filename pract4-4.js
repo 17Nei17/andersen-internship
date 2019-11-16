86a20fdcc25729fa27dd31ddb5fc06eca4e933bf
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-11-16 17:31:59 +0300

    Create pract4-4.js

---
+++ pract4-4.js
@@ -0,0 +1,26 @@
+/* 4. Напишите функцию высшего порядка myHiOrderFunction, которая принимает в качестве 1 аргумента другую функцию, 
+например, sayName, и применяет ее ко всем остальным аргументам.
+function sayName(name) {
+  console.log(`Я - ${name}`);
+};
+
+function miHiOrderFunction(func, ...arg) {
+    //ваш код
+};
+
+miHiOrderFunction(sayName, 'Вася'); // я - Вася
+miHiOrderFunction(sayName, 'Петя', 'Маша', 'John'); // Я -Петя Я - Маша ...
+miHiOrderFunction(sayName, 'Иван', 'Слава', 'Крис', 'Толя');
+*/
+function sayName(name) {
+  console.log(`Я - ${name}`);
+}
+
+function miHiOrderFunction(func, ...arg) {
+  for (let i=1; i<=arg.length; i++){
+    func(arguments[i]);
+  }
+}
+miHiOrderFunction(sayName, 'Петя', 'Маша', 'John'); // Я -Петя Я - Маша ...
+
+
