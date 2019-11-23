b2ca1b2e9c60943a44cbb52bce5678dad292c285
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-11-23 21:37:51 +0300

    Update pract5-2.js

--- pract5/pract5-2.js
+++ pract5/pract5-2.js
@@ -1,17 +1,27 @@
 /*Определить класс Children, который содержит такие поля (члены класса): закрытые – имя ребенка, фамилию и возраст,
  публичные – методы ввода данных и отображения их на экран. Создать два объекта класса, внести данные и показать их. */
-/*
+
+
 let Children = (name, surname, age) => {
   let newName = name;
   let newSurname = surname;
-  let newAge =age;
-  let addChildren = (newName,newSurname,newAge) =>{
-    return {newName,newSurname,newAge};
+  let newAge = age;
+  let getInfoChildren = function () {
+    console.log (`${newName} ${newSurname} ${newAge}`);
   }
-  const sayHello = () => console.log("Привет!");
-  return addChildren;
+  let setName = function (name) {
+    newName = name;
+  }
+  let setSurname= function (surname) {
+    newSurname = surname;
+  }
+  let seAge = function (age) {
+    newAge =age;
+  }
+  return {getInfoChildren ,setName,setSurname,seAge };
 };
-let vasia = Children (1,2,3);
-console.log(vasia.sayHello)
+let vasia = Children ('Василий','Васильевич',36);
 
-*/
+vasia.getInfoChildren();
+vasia.setName('Петя');
+vasia.getInfoChildren();
