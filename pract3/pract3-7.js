7511bdf2e97d65a3de42d795260a519a7c956840
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-06 14:15:45 +0300

    Update pract3-7.js

--- pract3/pract3-7.js
+++ pract3/pract3-7.js
@@ -1,4 +1,4 @@
-/*Напишите две функции, reverseArray и reverseArrayInPlace.
+/* Напишите две функции, reverseArray и reverseArrayInPlace.
   Первая получает массив как аргумент и выдаёт новый массив, с обратным порядком элементов.
   Вторая работает как оригинальный метод reverse – она меняет порядок элементов на обратный в том массиве,
   который был ей передан в качестве аргумента. Не используйте стандартный метод reverse.
@@ -10,24 +10,23 @@ console.log(arrayValue);
 // → [5, 4, 3, 2, 1]
 */
 
-function reverseArray (arrayValue){
-  let newArray= [];
-  for(let i = arrayValue.length-1; i>=0 ;i--){
-    newArray.push(arrayValue[i]); 
+function reverseArray(arrayValue) {
+  const newArray = [];
+  for (let i = arrayValue.length - 1; i >= 0; i -= 1) {
+    newArray.push(arrayValue[i]);
   }
   return newArray;
 }
 
-
-function reverseArrayInPlace(arrayValue){
-  for(let i = arrayValue.length-1; i>=0 ;i--){
-    arrayValue.push(arrayValue[i]);  
+function reverseArrayInPlace(arrayValue) {
+  for (let i = arrayValue.length - 1; i >= 0; i -= 1) {
+    arrayValue.push(arrayValue[i]);
   }
-  arrayValue.splice(0, arrayValue.length/2);
-  return arrayValue;  
+  arrayValue.splice(0, arrayValue.length / 2);
+  return arrayValue;
 }
 
-console.log(reverseArray(["A", "B", "C"]));
+console.log(reverseArray(['A', 'B', 'C']));
 // → ["C", "B", "A"];
 var arrayValue = [1, 2, 3, 4, 5];
 reverseArrayInPlace(arrayValue);
