27034494092ba53f728daa8e658cfea742861633
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-06 14:19:51 +0300

    Update pract4-9.js

--- pract4/pract4-9.js
+++ pract4/pract4-9.js
@@ -1,18 +1,18 @@
-/*9. Добавьте в Array.prototype метод myReducer(), возвращающий новый массив, 
+/* 9. Добавьте в Array.prototype метод myReducer(), возвращающий новый массив, 
 содержащий числа-элементы, делящиеся на 5 (решение аналогично предыдущей задаче). */
 
-let s = [23, 65, 98, 5];
-Array.prototype.myReducer = function(callback){
-  let newArray = [];
-  for (let i = 0; i<s.length; i++){
-    if (callback(s[i])===true){
+const s = [23, 65, 98, 5];
+Array.prototype.myReducer = function(callback) {
+  const newArray = [];
+  for (let i = 0; i < s.length; i += 1) {
+    if (callback(s[i]) === true) {
       newArray.push(s[i]);
     }
   }
   return newArray;
 };
 
-let new_s = s.myReducer(function(item){
-  return item % 5 == 0;
+const news = s.myReducer(function(item) {
+  return item % 5 === 0;
 });
-console.log(`${new_s}`);
+console.log(`${news}`);
