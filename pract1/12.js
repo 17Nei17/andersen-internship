ceb49d9bf3fcbb1d8f635e54d76ee9e0265732ed
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-06 14:03:59 +0300

    Update 12.js

--- pract1/12.js
+++ pract1/12.js
@@ -1,19 +1,18 @@
-//Вводится четырёхзначное число (abcd). Вывести сумму ab + cd.
-let A=5175;
-let sum1=0;
-let sum2=0;
-while (A>1){
-  for (let i=0;i<4;i++){
-    if (i<2){
-      sum1=sum1+(A%10);
-      A=Math.floor(A/10);    
-    }
-    else {
+// Вводится четырёхзначное число (abcd). Вывести сумму ab + cd.
+let A = 5175;
+let sum1 = 0;
+let sum2 = 0;
+while (A > 1) {
+  for (let i = 0; i < 4; i += 1) {
+    if (i < 2) {
+      sum1 += A % 10;
+      A = Math.floor(A / 10);
+    } else {
       Math.floor(sum2);
-      sum2+=A%10;
-      A=Math.floor(A/10); //округляет вниз
-    } 
+      sum2 += A % 10;
+      A = Math.floor(A / 10); // округляет вниз
+    }
   }
 }
-console.log("Результат ab+cd:");
-console.log(sum1+sum2);
+console.log('Результат ab+cd:');
+console.log(sum1 + sum2);
