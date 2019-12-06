2607f3365c6cbbf7d9d9df4c6790785a590d23cb
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-06 14:15:32 +0300

    Update pract3-2.js

--- pract3/pract3-2.js
+++ pract3/pract3-2.js
@@ -1,12 +1,12 @@
-//Создайте функцию, принимающую на входе целое число и возвращающую наибольшую цифру в этом числе.
+// Создайте функцию, принимающую на входе целое число и возвращающую наибольшую цифру в этом числе.
 let max = 0;
-function givNumeral (val){
-  let x = String(val);
-  for (let i=0;i<x.length;i++){
-    if (max<(+x[i])){
-      max=+x[i];
+function givNumeral(val) {
+  const x = String(val);
+  for (let i = 0; i < x.length; i += 1) {
+    if (max < +x[i]) {
+      max = +x[i];
     }
   }
-  console.log (max);
+  console.log(max);
 }
 givNumeral(2294802);
