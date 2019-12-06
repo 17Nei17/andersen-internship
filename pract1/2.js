8ee4bd7dad9c53f0a9368e9dbd77308900e3ef58
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-06 14:04:03 +0300

    Update 2.js

--- pract1/2.js
+++ pract1/2.js
@@ -1,5 +1,12 @@
-'use strict';
-let height=23;
-let width=10;
-let s= height*width;
+/* Напишите скрипт, который находит площадь прямоугольника высота 23см. (в
+числовую переменную height), шириной 10см (в числовую переменную width),
+значение площади должно храниться в числовой переменной s.
+*/
+
+const height = 23;
+const width = 10;
+function area(h, w) {
+  return h * w;
+}
+const s = area(height, width);
 console.log(s);
