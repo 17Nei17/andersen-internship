7424002097db1530b524117bca7f9827ae14db6a
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-06 14:08:01 +0300

    Update pract2-3.js

--- pract2/pract2-3.js
+++ pract2/pract2-3.js
@@ -1,10 +1,10 @@
-//Создать функцию вычисляющую путь по формуле S=v*t.
+// Создать функцию вычисляющую путь по формуле S=v*t.
 
-function way (v,t) {
-  let S=v*t;
+function way(v, t) {
+  const S = v * t;
   console.log(`${S}`);
 }
 
-way(10,2);
+way(10, 2);
 
-//let way = (v,t)=> v*t; 
+// let way = (v,t)=> v*t;
