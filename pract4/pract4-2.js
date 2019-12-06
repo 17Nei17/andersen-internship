be4c4333b2aaee3f1df72609a28a5bca70af50b5
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-06 14:19:35 +0300

    Update pract4-2.js

--- pract4/pract4-2.js
+++ pract4/pract4-2.js
@@ -1,4 +1,4 @@
-/*2. Имеются две базовых функции     prepareGreenTea (готовит чашку зеленого чая) и prepareBlackTea (готовит чашку черного чая), которые могут быть переданы в Функцию getTea для приготовления нужного количества чашек чая указанного вида. Допишите скрипт в указанном месте так, чтобы на выходе из него в переменной tea4GreenTeamFCC было 27 чашек зеленого чая, а в prepareBlackTea 13 чашек черного чая.
+/* 2. Имеются две базовых функции     prepareGreenTea (готовит чашку зеленого чая) и prepareBlackTea (готовит чашку черного чая), которые могут быть переданы в Функцию getTea для приготовления нужного количества чашек чая указанного вида. Допишите скрипт в указанном месте так, чтобы на выходе из него в переменной tea4GreenTeamFCC было 27 чашек зеленого чая, а в prepareBlackTea 13 чашек черного чая.
 const prepareGreenTea = () => 'greenTea';
 const prepareBlackTea = () => 'blackTea';
 const getTea = (prepareTea, numOfCups) => {
@@ -25,7 +25,7 @@ const prepareGreenTea = () => 'greenTea';
 const prepareBlackTea = () => 'blackTea';
 const getTea = (prepareTea, numOfCups) => {
   const teaCups = [];
-  for(let cups = 1; cups <= numOfCups; cups += 1) {
+  for (let cups = 1; cups <= numOfCups; cups += 1) {
     const teaCup = prepareTea();
     teaCups.push(teaCup);
   }
@@ -37,7 +37,4 @@ const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
 const tea4BlackTeamFCC = getTea(prepareBlackTea, 14);
 // Add your code above this line
 
-console.log(
-  tea4GreenTeamFCC,
-  tea4BlackTeamFCC
-);
+console.log(tea4GreenTeamFCC, tea4BlackTeamFCC);
