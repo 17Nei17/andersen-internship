03f7b1071e3795eb8315a6312265c039287f3fc3
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-06 14:22:38 +0300

    Update pract5-6.js

--- pract5/pract5-6.js
+++ pract5/pract5-6.js
@@ -1,10 +1,10 @@
-/*6. Класс Покупатель: Фамилия, Имя, Отчество, Адрес, Номер кредитной карточки, Номер банковского счета; Конструктор; 
+/* 6. Класс Покупатель: Фамилия, Имя, Отчество, Адрес, Номер кредитной карточки, Номер банковского счета; Конструктор; 
 Методы: установка значений атрибутов, получение значений атрибутов, вывод информации. 
 Создать массив объектов данного класса. 
 Вывести список покупателей в алфавитном порядке и список покупателей, у которых номер кредитной карточки находится в заданном диапазоне. */
 
 class Customer {
-  constructor(surname,name,midname,adress, numberCard, numberBankAcc){
+  constructor(surname, name, midname, adress, numberCard, numberBankAcc) {
     this.surname = surname;
     this.name = name;
     this.midname = midname;
@@ -12,7 +12,8 @@ class Customer {
     this.numberCard = numberCard;
     this.numberBankAcc = numberBankAcc;
   }
-  setAllCustomer(surname,name,midname,adress, numberCard, numberBankAcc){
+
+  setAllCustomer(surname, name, midname, adress, numberCard, numberBankAcc) {
     this.surname = surname;
     this.name = name;
     this.midname = midname;
@@ -20,46 +21,55 @@ class Customer {
     this.numberCard = numberCard;
     this.numberBankAcc = numberBankAcc;
   }
-  setCustomerSurname(surname){
+
+  setCustomerSurname(surname) {
     this.surname = surname;
   }
-  setCustomerName(name){
+
+  setCustomerName(name) {
     this.name = name;
   }
-  setCustomerMidname(midname){
+
+  setCustomerMidname(midname) {
     this.midname = midname;
   }
-  setCustomerAdress(adress){
+
+  setCustomerAdress(adress) {
     this.adress = adress;
   }
-  setCustomerNumberCard(numberCard){
+
+  setCustomerNumberCard(numberCard) {
     this.numberCard = numberCard;
   }
-  setCustomerNumberBankAcc(numberBankAcc){
+
+  setCustomerNumberBankAcc(numberBankAcc) {
     this.numberBankAcc = numberBankAcc;
   }
-  viewCustomer(){
-    console.log (`${this.surname},${this.name},${this.midname},${this.adress}, ${this.numberCard}, ${this.numberBankAcc}`)
+
+  viewCustomer() {
+    console.log(
+      `${this.surname},${this.name},${this.midname},${this.adress}, ${this.numberCard}, ${this.numberBankAcc}`
+    );
   }
 }
 
-let customer1 = new Customer();
-customer1.setAllCustomer('Новак','Светлана','Сергеевна','город Мозырь',100,123321);
-let customer2 = new Customer('Пунтус','Руслан','Леонидович','город Мозырь', 210,985743);
-let customer3 = new Customer('Блоцкая','Дарья','Степановна','город Мозырь', 295,547263);
-let arrCustomers = [customer1,customer2,customer3];
+const customer1 = new Customer();
+customer1.setAllCustomer('Новак', 'Светлана', 'Сергеевна', 'город Мозырь', 100, 123321);
+const customer2 = new Customer('Пунтус', 'Руслан', 'Леонидович', 'город Мозырь', 210, 985743);
+const customer3 = new Customer('Блоцкая', 'Дарья', 'Степановна', 'город Мозырь', 295, 547263);
+const arrCustomers = [customer1, customer2, customer3];
 
-arrCustomers.sort(function(a, b){//Вывести список покупателей в алфавитном порядке
+arrCustomers.sort(function(a, b) {
+  // Вывести список покупателей в алфавитном порядке
   return a.surname > b.surname;
 });
-console.log ('Список покупателей по алфавитному порядку (сортировка по фамилии) :');
-console.log (arrCustomers);
-//список покупателей, у которых номер кредитной карточки находится в заданном диапазоне.
+console.log('Список покупателей по алфавитному порядку (сортировка по фамилии) :');
+console.log(arrCustomers);
+// список покупателей, у которых номер кредитной карточки находится в заданном диапазоне.
 
-console.log ('Покупатели у которых номер кредитной карты 300>x>200 :');
-for (let i of arrCustomers){
-  if (i.numberCard>200&&i.numberCard<300){
+console.log('Покупатели у которых номер кредитной карты 300>x>200 :');
+for (const i of arrCustomers) {
+  if (i.numberCard > 200 && i.numberCard < 300) {
     console.log(i);
   }
 }
-
