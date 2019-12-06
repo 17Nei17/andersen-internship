06daa9533cfa6b86c6f3bda94bc2d85080edf4ad
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-06 14:08:11 +0300

    Update pract2-7.js

--- pract2/pract2-7.js
+++ pract2/pract2-7.js
@@ -1,8 +1,6 @@
-//С помощью встроенного объекта Date получить текущую дату и вычислить дату, которая наступит через 100 дней.function now() {
+// С помощью встроенного объекта Date получить текущую дату и вычислить дату, которая наступит через 100 дней.function now() {
 
-let nowDate = new Date();
-console.log (`${nowDate}`);
-nowDate.setDate(nowDate.getDate()+100);
-console.log (`${nowDate}`);
-
-
+const nowDate = new Date();
+console.log(`${nowDate}`);
+nowDate.setDate(nowDate.getDate() + 100);
+console.log(`${nowDate}`);
