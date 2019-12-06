4e32d61f45d13995e14a2fc6473f631036075919
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-06 14:22:30 +0300

    Update pract5-2.js

--- pract5/pract5-2.js
+++ pract5/pract5-2.js
@@ -1,28 +1,26 @@
-/*Определить класс Children, который содержит такие поля (члены класса): закрытые – имя ребенка, фамилию и возраст,
+/* Определить класс Children, который содержит такие поля (члены класса): закрытые – имя ребенка, фамилию и возраст,
  публичные – методы ввода данных и отображения их на экран. Создать два объекта класса, внести данные и показать их. */
 
-
-let Children = (name, surname, age) => {
+const Children = (name, surname, age) => {
   let newName = name;
   let newSurname = surname;
   let newAge = age;
-  let getInfoChildren = function () {
-    console.log (`${newName} ${newSurname} ${newAge}`);
-  }
-  let setName = function (name) {
+  const getInfoChildren = function() {
+    console.log(`${newName} ${newSurname} ${newAge}`);
+  };
+  const setName = function(name) {
     newName = name;
-  }
-  let setSurname= function (surname) {
+  };
+  const setSurname = function(surname) {
     newSurname = surname;
-  }
-  let seAge = function (age) {
+  };
+  const seAge = function(age) {
     newAge = age;
-  }
-  return {getInfoChildren ,setName,setSurname,seAge };
+  };
+  return { getInfoChildren, setName, setSurname, seAge };
 };
-let vasia = Children ('Василий','Васильевич',36);
-let sanya = Children ('Александр', 'Александрович', 22)
+const vasia = Children('Василий', 'Васильевич', 36);
+const sanya = Children('Александр', 'Александрович', 22);
 vasia.getInfoChildren();
 sanya.getInfoChildren();
 vasia.setName('Петя');
-
