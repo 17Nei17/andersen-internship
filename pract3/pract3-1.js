41f8266ce3db4947f9cf05f057a04dd98be5e076
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-06 14:15:15 +0300

    починила с prettier

--- pract3/pract3-1.js
+++ pract3/pract3-1.js
@@ -1,7 +1,7 @@
-//Создайте функцию, принимающую на входе целое число и возвращающую количество цифр  в этом числе.
+// Создайте функцию, принимающую на входе целое число и возвращающую количество цифр  в этом числе.
 
-function givNumeral (x){
-  let numeral = String(x);
-  console.log (`${numeral.length}`);
+function givNumeral(x) {
+  const numeral = String(x);
+  console.log(`${numeral.length}`);
 }
 givNumeral(325443);
