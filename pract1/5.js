6b226afa3c89da4a71d105916d767bc43a731703
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-06 14:04:10 +0300

    Update 5.js

--- pract1/5.js
+++ pract1/5.js
@@ -1,13 +1,11 @@
-//Среди трех чисел найти среднее. Если среди чисел есть равные, вывести сообщение Ошибка;.
-let num1,num2,num3;
-num1=20;
-num2=10;
-num3=30;
-if ((num1==num2)||(num1==num3)||(num2==num1)||(num2==num3)){
-  console.log ("Ошибка");
-}
-else {
-  let s=(num1+num2+num3)/3;
+// Среди трех чисел найти среднее. Если среди чисел есть равные, вывести сообщение Ошибка;.
+
+const num1 = 20;
+const num2 = 10;
+const num3 = 30;
+if (num1 === num2 || num1 === num3 || num2 === num1 || num2 === num3) {
+  console.log('Ошибка');
+} else {
+  const s = (num1 + num2 + num3) / 3;
   console.log(s);
 }
-
