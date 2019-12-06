9d06512666f30f370a86b8eb4482f9954788edd4
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-06 14:04:19 +0300

    Update 8.js

--- pract1/8.js
+++ pract1/8.js
@@ -1,11 +1,11 @@
-//Найти сумму чисел от 100 до 200 кратных 17
+// Найти сумму чисел от 100 до 200 кратных 17
 
-let sum=0;
-let start=100;
-let end=200;
-for(let i=start;i<=end;i++){
-  if (i%17==0){
-    sum=sum+i;
+let sum = 0;
+const start = 100;
+const end = 200;
+for (let i = start; i <= end; i += 1) {
+  if (i % 17 === 0) {
+    sum += i;
   }
 }
 console.log(sum);
