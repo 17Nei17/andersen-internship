657145fd61f00974654f4f9dd1939a7e535356d8
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-06 14:19:32 +0300

    Update pract4-10.js

--- pract4/pract4-10.js
+++ pract4/pract4-10.js
@@ -1,16 +1,16 @@
-/*10. Напишите свой собственный Array.prototype.myMap(), который должен вести себя точно так же, как Array.prototype.map().
+/* 10. Напишите свой собственный Array.prototype.myMap(), который должен вести себя точно так же, как Array.prototype.map().
  Можно использовать цикл for или метод Array.prototype.forEach (). 
  Решение аналогично задачам 8-9. */
 
-let s = [23, 65, 98, 5];
-Array.prototype.myMap = function(callback){
-  let newArray = [];
-  for (let i = 0; i<s.length; i++){
-    callback(newArray.push(s[i]*2)); //значение массива*2
+const s = [23, 65, 98, 5];
+Array.prototype.myMap = function(callback) {
+  const newArray = [];
+  for (let i = 0; i < s.length; i += 1) {
+    callback(newArray.push(s[i] * 2)); // значение массива*2
   }
   return newArray;
 };
-let new_s = s.myMap(function(item){
+const news = s.myMap(function(item) {
   return item;
 });
-console.log(`${new_s}`);
+console.log(`${news}`);
