13b46dac6babd5a16a89fcd3a012cf258881a483
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-06 14:04:13 +0300

    Update 6.js

--- pract1/6.js
+++ pract1/6.js
@@ -1,7 +1,6 @@
-//Вывести на экран кубы чисел от A до B,
-let A=3;
-let B=5;
-for(let i=A;i<=B;i++){
-  //  console.log(i*i*i);
-  console.log(Math.pow(i,3));
+// Вывести на экран кубы чисел от A до B,
+const A = 3;
+const B = 5;
+for (let i = A; i <= B; i += 1) {
+  console.log(i ** 3);
 }
