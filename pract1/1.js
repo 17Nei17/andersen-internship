85c5ca294b4f2608cb2d48dbb50a777331c0d9f9
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-06 14:02:44 +0300

    Update 1.js

--- pract1/1.js
+++ pract1/1.js
@@ -1,14 +1,12 @@
+// Определите переменные str, num, flag и txt со значениями «Привет», 123, true,
+// «true». При помощи оператора определения типа убедитесь, что переменных
+// принадлежат типам: string, number, boolean.
 
-//Определите переменные str, num, flag и txt со значениями «Привет», 123, true,
-//«true». При помощи оператора определения типа убедитесь, что переменных
-//принадлежат типам: string, number, boolean.
-'use strict';
-let str, num,flag, txt;
-str = "Привет";
-num=123;
-flag=true;
-txt="true";
-console.log(typeof(str));
-console.log(typeof(num));
-console.log(typeof(flag));
-console.log(typeof(txt));
+const str = 'Привет';
+const num = 123;
+const flag = true;
+const txt = 'true';
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);
