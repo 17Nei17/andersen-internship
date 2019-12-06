0beecdb8559d4ff53f47a45295d9a022060c2705
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-06 14:19:48 +0300

    Update pract4-8.js

--- pract4/pract4-8.js
+++ pract4/pract4-8.js
@@ -1,4 +1,4 @@
-/*8. Напишите свой собственный Array.prototype.myFilter (), 
+/* 8. Напишите свой собственный Array.prototype.myFilter (), 
 который должен вести себя точно так же, как Array.prototype.filter().
  Можно использовать цикл for или метод Array.prototype.forEach ().
 let s = [23, 65, 98, 5];
@@ -15,18 +15,18 @@ let new_s = s.myFilter(function(item){
 console.log(new_s);
 */
 
-let s = [23, 65, 98, 5];
-Array.prototype.myFilter = function(callback){
-  let newArray = [];
-  for (let i = 0; i<s.length; i++){
-    if (callback(s[i])===true){
+const s = [23, 65, 98, 5];
+Array.prototype.myFilter = function(callback) {
+  const newArray = [];
+  for (let i = 0; i < s.length; i += 1) {
+    if (callback(s[i]) === true) {
       newArray.push(s[i]);
     }
   }
   return newArray;
 };
 
-let new_s = s.myFilter(function(item){
-  return item % 2 == 1;
+const news = s.myFilter(function(item) {
+  return item % 2 === 1;
 });
-console.log(`${new_s}`);
+console.log(`${news}`);
