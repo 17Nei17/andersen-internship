9b1b71a4f9d95a4612ef48b6ed65e10800040a2a
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2020-01-16 09:19:36 +0300

    Update pract3-3.js

--- pract3/pract3-3.js
+++ pract3/pract3-3.js
@@ -1,12 +1,13 @@
 // Напишите функцию, которая в качестве параметра получает целое число и возвращает true,
 // если число простое и false, если нет.
-let flags = true;
-function getSimple(x) {
-  for (let i = 2; i < x; i += 1) {
-    if (x % i !== 0) {
-      continue;
-    } else flags = false;
+
+function isSimple(n) {
+  if (n === 1) {
+    return false;
   }
-  console.log(`${flags}`);
+  for (let i = 2; i * i <= n; i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
-getSimple(4);
+console.log(isSimple(2));
