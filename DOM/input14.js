a6548bba4e978c0d94446a2fa3ec2a4dd5c8ad6f
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2020-01-16 15:14:28 +0300

    Delete input14.js

--- DOM/input14.js
+++
@@ -1,5 +0,0 @@
-// Создайте форму с input, при вводе данных, под Input они повторяются (без кнопки, событие на изменение содержимого).
-const input = document.forms.form5.fname;
-input.oninput = function() {
-  document.getElementById('result').innerHTML = input.value;
-};
