2178c08353809f553233512e0660b035376a4085
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2020-01-16 15:12:47 +0300

    Delete form.js

--- DOM/form.js
+++
@@ -1,4 +0,0 @@
-// Напишите функцию JavaScript, возвращающую значения имени и фамилии из следующей формы:
-function getDataFromForm() {
-  alert(document.forms.form1.fname.value + ' ' + document.forms.form1.lname.value);
-}
