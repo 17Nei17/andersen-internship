ac38c1fbebc10fa0d7a0865098f62ffcb0e2b3b9
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2020-01-16 15:03:43 +0300

    продублировала описание задания и в html и в js скрипт

--- DOM/switchInput.js
+++ DOM/switchInput.js
@@ -1,3 +1,4 @@
+// Создайте форму с двумя input и кнопкой. По нажатию на кнопку содержимое input элементов меняется местами.
 function switchInput() {
   const input1 = document.forms.form2.fname.value;
   const input2 = document.forms.form2.lname.value;
