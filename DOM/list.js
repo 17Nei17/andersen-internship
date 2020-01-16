7dcafdcc18bc19ae3c5e94cffb6940e7886c7efc
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2020-01-16 15:14:30 +0300

    Delete list.js

--- DOM/list.js
+++
@@ -1,6 +0,0 @@
-// Напишите функцию JavaScript, удаляющую указанный элемент из выпадающего списка формы.
-function deleteElement() {
-  const select = document.getElementById('select');
-  const i = prompt('Номер строки?');
-  select[i - 1].remove();
-}
