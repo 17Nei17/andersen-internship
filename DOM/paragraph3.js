db81f0ada81f0cd27dae0faee698dbd6749e3b3e
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2020-01-16 15:03:36 +0300

    продублировала описание задания и в html и в js скрипт

--- DOM/paragraph3.js
+++ DOM/paragraph3.js
@@ -1,3 +1,8 @@
+/* <p>В следующем HTML-коде замените содержимое элементов с классом zzz на их порядковый номер в коде.</p>
+  <h2 class="zzz">Заголовок с классом zzz.</h2>
+  <p class="zzz">Абзац с классом zzz.</p>
+  <p class="zzz">Абзац с классом zzz.</p>
+  <p>Просто абзац, не поменяется.</p> */
 function paragraph3() {
   for (let i = 0; i < document.getElementsByClassName('zzz').length; i++) {
     document.getElementsByClassName('zzz')[i].innerHTML = i;
