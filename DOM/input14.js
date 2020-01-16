b9408d896c908f0afa981295047c31f8d62fc586
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2020-01-16 15:03:22 +0300

    продублировала описание задания и в html и в js скрипт

--- DOM/input14.js
+++ DOM/input14.js
@@ -1,3 +1,4 @@
+// Создайте форму с input, при вводе данных, под Input они повторяются (без кнопки, событие на изменение содержимого).
 const input = document.forms.form5.fname;
 input.oninput = function() {
   document.getElementById('result').innerHTML = input.value;
