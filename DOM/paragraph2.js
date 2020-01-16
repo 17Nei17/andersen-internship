689002c193b3e48a2892b43df4628c02e6caa90f
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2020-01-16 15:03:30 +0300

    продублировала описание задания и в html и в js скрипт

--- DOM/paragraph2.js
+++ DOM/paragraph2.js
@@ -1,3 +1,8 @@
+/* <p>В следующем HTML-коде замените содержимое абзацев на их порядковый номер в коде.</p>
+<h2>Заголовок, не поменяется.</h2>
+<p>Абзац, поменяется.</p>
+<p>Абзац, поменяется.</p>
+<p>Абзац, поменяется.</p> */
 function paragraph2() {
   for (let i = 1; i < document.getElementsByTagName('p').length; i++) {
     document.getElementsByTagName('DIV')[0].getElementsByTagName('p')[i].innerHTML = i;
