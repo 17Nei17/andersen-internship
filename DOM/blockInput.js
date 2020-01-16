ef13137482aa26681951c7ed87ee2b6c39484aa6
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2020-01-16 15:03:01 +0300

    продублировала описание задания и в html и в js скрипт

--- DOM/blockInput.js
+++ DOM/blockInput.js
@@ -1,3 +1,4 @@
+// Создайте форму с input и двумя кнопками. Нажатие на первую кнопку блокирует возможность изменения содержимого input, а вторая - разблокирует
 function blockInput() {
   const input = document.forms.form4.fname.value;
   form4.fname.outerHTML = `<input type = "text" name = "fname" value = "${input}" readonly>`;
